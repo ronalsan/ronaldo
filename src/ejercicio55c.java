6b6928f6e0130e858c5e8232629cd2c0c779bc8c
ronalsan <160769053+ronalsan@users.noreply.github.com>
2025-03-21 17:11:14 -0500

    Add files via upload

---
+++ src/ejercicio55c.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio55c {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Escriba un numero");
+        int numero1 = scanner.nextInt();
+        System.out.println("Escriba otro numero");
+        int numero2 = scanner.nextInt();
+        if (numero1>numero2) {
+            System.out.println(numero1+" es numero mayor que "+numero2);
+
+        } 
+        if (numero1<numero2) {
+            System.out.println(numero2+" es mayor que "+numero1);
+        }
+            
+        
+    }
+}
