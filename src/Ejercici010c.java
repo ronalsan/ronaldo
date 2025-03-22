7938f3cc0dc526090a9e7121a9276ac767a7f222
ronalsan <160769053+ronalsan@users.noreply.github.com>
2025-03-22 00:02:17 -0500

    Add files via upload

---
+++ src/Ejercici010c.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercici010c {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.println("Digite numero de preguntas");
+       float preguntas = input.nextFloat();
+       System.out.println("Digite respuestas correctas");
+       float correctas = input.nextFloat();
+
+       float division= (correctas/preguntas);
+       float porcentaje=(division*100);
+
+       if (porcentaje<50) {
+        System.out.println(porcentaje+" esta fuera de nivel");
+       }else if (porcentaje<75) {
+        System.out.println(porcentaje+" nivel regular");
+       }else if (porcentaje<90) {
+        System.out.println(porcentaje+" nivel medio");
+       }else if (porcentaje>90) {
+        System.out.println(porcentaje+" nivel maximo");
+       }
+    }
+}
