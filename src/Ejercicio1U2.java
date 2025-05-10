2c4982a8cf49e5606b25abed95e842da012ac79b
ronalsan <160769053+ronalsan@users.noreply.github.com>
2025-05-10 12:02:40 -0500

    Add files via upload

---
+++ src/Ejercicio1U2.java
@@ -0,0 +1,13 @@
+public class Ejercicio1U2 {
+    public class main {
+    public static void main(String[] args) {
+        int valor=10;
+        cambiarvalor(valor);
+        System.out.println("valor despues de cambiar un valor"+valor);
+    }
+    public static void cambiarvalor(int valor){
+        valor=20;
+    }
+        
+    }
+}
